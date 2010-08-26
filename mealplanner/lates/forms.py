edf1e35fbaceba36f9d6462d8900fa64fcd716cd
Jon Evans <cdawzrd@gmail.com>
2010-08-25 23:36:39 -0400

    bugfix in lates.forms

--- mealplanner/lates/forms.py
+++ mealplanner/lates/forms.py
@@ -8,7 +8,7 @@ from utils import getWeekdayStr
 class LateSubmitForm(forms.Form):
     name = forms.CharField(max_length=20)
     email = forms.EmailField(max_length=200)
-    message = forms.CharField(max_length=500,required=False)
+    message = forms.CharField(required=False,max_length=500)
 
     type = forms.ChoiceField(required=True,widget=forms.RadioSelect(),choices=[("all","no matter when dinner is served."),("early","only if dinner will be served before 7pm")])
     schedule = forms.ChoiceField(required=True,widget=forms.RadioSelect(),choices=[("today","today only."),("weekday","every week on "+getWeekdayStr(datetime.datetime.now())+"s")])
